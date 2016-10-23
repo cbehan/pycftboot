56ceeffc7797b94d5f6013dd84530b7c914a1f67
Connor Behan <connor.behan@gmail.com>
2016-10-22 20:12:02 -0400

    Value readability
    When the coefficients are kept in a somewhat factored form, it will be
    easier for people to find mistakes, as I believe I already have.
    Amusingly, the longest line has gone from 789 to 268 characters.
    
    Signed-off-by: Connor Behan <connor.behan@gmail.com>

--- blocks2.py
+++ blocks2.py
@@ -14,7 +14,7 @@ def convert_table(tab_short, tab_long):
     """
     for l in range(0, len(tab_short.table)):
         pole_prod = 1
-        small_list = short_table[l].poles[:]
+        small_list = tab_short.table[l].poles[:]
 
         for p in tab_long.table[l].poles:
             index = get_index_approx(small_list, p)
@@ -123,47 +123,46 @@ class ConformalBlockTableSeed2:
             self.table.append(PolynomialVector([], [l, 0], pole_set[l // step]))
 
             for k in range(1, k_max + 1):
-                # In the identical scalar case, there would be only three
-                # The min(k, 7) would also be min(k, 3)
+                # A good check is to force this code to run for identical scalars too
+                # This should produce the same blocks as the shorter recursion coming up
                 recursion_coeffs = [0, 0, 0, 0, 0, 0, 0]
-
-                # These have been copied from JuliBoots
-                # Explicitly including c_2 and c_4 could aid brevity
-                recursion_coeffs[0] += (k ** 4) * (-1)
-                recursion_coeffs[0] += (k ** 3) * 4 * (2 - delta + 4 * delta_sum + nu)
-                recursion_coeffs[0] += (k ** 2) * (l * l + 2 * l * nu - 4 * nu * nu - 22 * nu - 23 - delta * (5 * delta - 10 * nu - 22) + 24 * delta_sum * (2 * delta - 2 * nu - 3) + 8 * delta_prod)
-                recursion_coeffs[0] += (k ** 1) * 2 * ((delta - nu - 2) * (l * l - 7 - delta * (delta - 6) + 2 * nu * (delta + l - 3)) + 4 * delta_sum * (l * l - 4 * nu * nu - 13 - delta * (5 * delta - 16) + 2 * nu * (5 * delta + l - 8)) - 2 * delta_prod * (4 * delta - 6 * nu - 5))
-                recursion_coeffs[0] += (k ** 0) * (3 + 2 * nu - 2 * delta) * (4 * delta_prod * (1 + 4 * nu - delta) - (4 * delta_sum + 1) * (delta + l - 2) * (delta - l - 2 * nu - 2))
-                recursion_coeffs[1] += (k ** 4) * 3
-                recursion_coeffs[1] += (k ** 3) * 4 * (3 * delta - nu - 7 + 4 * delta_sum)
-                recursion_coeffs[1] += (k ** 2) * (3 * delta * (5 * delta - 2 * nu - 26) + 2 * nu * (11 - 3 * l - 2 * nu) - 3 * l * l + 107 - 8 * delta_prod - 8 * delta_sum * (10 * delta_sum + 15 - 6 * nu - 6 * delta))
-                recursion_coeffs[1] += (k ** 1) * 2 * (3 * delta * delta * delta + delta * delta * (nu - 29) - 3 * delta * (2 * nu * (nu + l - 1) + l * l - 31) - nu * (23 - 14 * l - l * l - 14 * nu - 2 * l * nu) + 7 * l * l - 97 + 2 * delta_prod * (16 * delta_sum + 4 * delta - 10 * nu - 79) + 8 * delta_sum * delta_sum * (10 * delta - 8 * nu - 21) + 4 * delta_sum * (delta * (28 - 5 * delta) + 2 * nu * (5 * delta + l - 14) - 4 * nu * nu + l * l - 37))
-                recursion_coeffs[1] += (k ** 0) * 4 * (delta * delta * delta * (nu - 2) - delta * delta * (2 * nu * nu + 3 * nu - 15) - delta * (l * (l + 2 * nu) * (nu - 2) - 8 * nu * nu + nu + 39) + 34 - 4 * l * l + nu * (2 * l * nu - 10 * nu + l * l - 8 * l + 9) + delta_prod * (4 * delta_sum * (7 + 6 * nu - 4 * delta) - 18 * delta * delta + delta * (34 * nu + 79) - nu * (12 * nu - 8 * l + 74) + 4 * l * l - 86) + delta_sum * (5 + 2 * nu - 2 * delta) * (l * l + 2 * nu * (delta + l - 4) - (delta - 2) * (delta - 6)) + 4 * (delta_sum * delta_sum - delta_prod) * (4 * delta * delta - delta * (19 + 6 * nu) - 2 * nu * (l - 8) + l * l - 22))
-                recursion_coeffs[2] += (k ** 4) * 3
-                recursion_coeffs[2] += (k ** 3) * 4 * (3 * delta - nu - 10 - 8 * delta_sum)
-                recursion_coeffs[2] += (k ** 2) * (3 * delta * (5 * delta - 2 * nu - 38) - 2 * nu * (2 * nu + 3 * l - 17) - 3 * l * l + 209 - 16 * delta_prod - 16 * delta_sum * (5 * delta_sum + 6 * delta - 18))
-                recursion_coeffs[2] += (k ** 1) * 2 * (3 * delta * delta * delta + delta * delta * (nu - 44) - 3 * delta * (2 * nu * (nu + l - 2) + l * l - 63) + nu * (2 * l * nu + 18 * nu + l * l + 20 * l - 51) + 10 * l * l - 252 + 32 * delta_sum * (delta_prod + 2 * delta_sum * delta_sum) + 8 * (delta_sum * delta_sum - delta_prod) * (31 + 8 * nu - 10 * delta) - 8 * delta_sum * (5 * delta * delta + 2 * delta * (nu - 17) - 2 * nu * (2 * nu + l) - l * l + 57))
-                recursion_coeffs[2] += (k ** 0) * (2 * delta * delta * delta * (2 * nu - 7) + delta * delta * (133 - 14 * nu - 8 * nu) - 2 * delta * (2 * nu * nu * (2 * l - 11) + nu * (2 * l * l - 14 * l + 11) - 7 * l * l + 216) + nu * (4 * l * nu - 72 * nu + 2 * l * l - 66 * l + 108) - 33 * l * l + 468 + 64 * delta_sum * (2 * delta_sum * delta_sum - 3 * delta_prod) * (delta - 3) + 16 * (delta_sum * delta_sum - delta_prod) * (8 * delta_prod + delta * (29 + 6 * nu - 4 * delta) + 2 * nu * (l - 12) + l * l - 48) + 16 * delta_sum * (delta - 3) * (l * l - (delta - 3) * (delta - 7) - 2 * nu * (delta - l) + 4 * nu * nu) + 16 * delta_prod * delta_sum * (16 * delta - 10 * nu - 41) + 8 * delta_prod * (delta * (73 + 10 * nu - 10 * delta) + 2 * nu * (4 * nu + 2 * l - 27) + 2 * l * l - 123))
-                recursion_coeffs[3] += (k ** 4) * (-3)
-                recursion_coeffs[3] += (k ** 3) * 4 * (11 - nu - 3 * delta - 8 * delta_sum)
-                recursion_coeffs[3] += (k ** 2) * (-15 * delta * delta - 18 * delta * (nu - 7) + nu * (4 * nu + 6 * l + 50) + 3 * l * l - 251 + delta_sum * (80 * delta_sum + 96 * (4 - delta)) + 16 * delta_prod)
-                recursion_coeffs[3] += (k ** 1) * 2 * (-3 * delta * delta * delta + (49 - 11 * nu) * delta * delta + (3 * l * l + 2 * nu * (nu + 3 * l + 35) - 229) * delta + nu * (2 * l * nu - 10 * nu + l * l - 22 * l - 107) - 11 * l * l + 329 + 64 * delta_sum * delta_sum * delta_sum + 4 * delta_prod * (24 * delta + 14 * nu - 91 - 56 * delta_sum) + 8 * delta_sum * (delta * (46 - 5 * delta - 2 * nu) + 2 * nu * (2 * nu + l) + l * l - 99) + 8 * (delta_sum * delta_sum - delta_prod) * (10 * delta + 8 * nu - 39))
-                recursion_coeffs[3] += (k ** 0) * (-4) * (2 * delta * delta * delta * (nu - 2) + delta * delta * (41 - 20 * nu) + delta * (2 * nu * (35 - l * l + 4 * l - 2 * l * nu) + 4 * l * l - 143) + 2 * (nu * (4 * l * nu - 2 * nu + 2 * l * l - 10 * l - 39) - 5 * l * l + 83) + 2 * delta_prod * (16 * delta_sum * delta_sum + delta * (87 - 18 * nu - 10 * delta) + 4 * nu * (2 * nu + l + 11) + 2 * l * l - 172) + 16 * (4 - delta) * delta_sum * (2 * delta_sum * delta_sum - 3 * delta_prod) + 4 * (delta_sum * delta_sum - delta_prod) * (delta * (37 - 10 * nu - 4 * delta) + 2 * nu * (l + 16) + l * l - 76) + 4 * delta_prod * delta_sum * (16 * delta + 10 * nu - 71) - 4 * delta_sum * (4 - delta) * (delta * (delta + 2 * nu - 14) - 2 * nu * (2 * nu + l) - l * l + 35))
-                recursion_coeffs[4] += (k ** 4) * (-3)
-                recursion_coeffs[4] += (k ** 3) * 4 * (14 - nu - 3 * delta + 4 * delta_sum)
-                recursion_coeffs[4] += (k ** 2) * (-15 * delta * delta - 18 * delta * (nu - 9) + 2 * nu * (2 * nu + 3 * l + 31) + 3 * l * l - 401 + 80 * delta_sum * delta_sum + 24 * delta_sum * (2 * delta + 2 * nu - 9) + 8 * delta_prod)
-                recursion_coeffs[4] += (k ** 1) * (-2) * (delta * delta * (3 * delta + 11 * nu - 64) - delta * (2 * nu * (nu + 3 * l + 44) + 3 * l * l - 373) + nu * (14 * nu - 2 * l * nu - l * l + 28 * l + 163) + 14 * l * l - 652 + 32 * delta_prod * delta_sum + 8 * (delta_sum * delta_sum - delta_prod) * (49 - 8 * nu - 10 * delta) + 4 * delta_sum * (delta * (52 - 14 * nu - 5 * delta) - 2 * nu * (2 * nu - 28 - l) + l * l - 121) + 2 * delta_prod * (221 - 42 * nu - 44 * delta))
-                recursion_coeffs[4] += (k ** 0) * (2 * delta * delta * delta * (11 - 4 * nu) + delta * delta * (102 * nu - 277) + 2 * delta * (nu * (8 * l * nu - 2 * nu + 4 * l * l - 22 * l - 219) - 11 * l * l + 584) + 2 * nu * (20 * nu - 18 * l * nu - 9 * l * l + 65 * l + 290) + 65 * l * l - 1620 - 16 * (delta_sum * delta_sum - delta_prod) * (delta * (47 - 10 * nu - 4 * delta) + 2 * nu * (20 + l) + l * l - 120) + 4 * delta_sum * (2 * delta + 2 * nu - 9) * (delta * (delta + 6 * nu - 16) - 2 * nu * (l + 10) - l * l + 40) - 16 * delta_prod * delta_sum * (4 * delta + 6 * nu - 21) + 4 * delta_prod * (delta * (18 * delta + 50 * nu - 213) + 4 * nu * (3 * nu - 2 * l - 55) - 4 * l * l + 555))
-                recursion_coeffs[5] += (k ** 4)
-                recursion_coeffs[5] += (k ** 3) * 4 * (delta + nu - 5 + 4 * delta_sum)
-                recursion_coeffs[5] += (k ** 2) * (5 * delta * delta + 2 * (7 * nu - 29) * delta + 2 * nu * (2 * nu - l - 31) - l * l + 149 - 24 * delta_sum * (11 - 2 * nu - 2 * delta) - 8 * delta_prod)
-                recursion_coeffs[5] += (k ** 1) * (-2) * ((delta + nu - 5) * (delta * (18 - 6 * nu - delta) + 2 * nu * (l + 11) + l * l - 49) - 4 * delta_sum * (delta * (5 * delta + 14 * nu - 64) + 2 * nu * (2 * nu - l - 34) - l * l + 181) + 2 * delta_prod * (4 * delta + 6 * nu - 23))
-                recursion_coeffs[5] += (k ** 0) * 4 * ((nu - 2) * (delta - l - 5) * (delta - 3) * (delta + 2 * nu + l - 5) + (2 * delta + 2 * nu - 11) * (delta_sum * (delta * (delta + 6 * nu - 20) - 2 * nu * (l + 12) - l * l + 60) - delta_prod * (delta + 2 * nu - 6)))
-                recursion_coeffs[6] += (k ** 4)
-                recursion_coeffs[6] += (k ** 3) * 4 * (delta + nu - 6)
-                recursion_coeffs[6] += (k ** 2) * (delta * (5 * delta + 14 * nu - 70) + 2 * nu * (2 * nu - l - 37) - l * l + 215)
-                recursion_coeffs[6] += (k ** 1) * (-2) * (delta + nu - 6) * (delta * (22 - 6 * nu - delta) + 2 * nu * (l + 13) + l * l - 71)
-                recursion_coeffs[6] += (k ** 0) * (2 * nu - 5) * (delta - l - 6) * (2 * delta - 7) * (delta + 2 * nu + l - 6)
+                recursion_coeffs[0] += 2 * c_2 * (2 * nu + 1) * (4 * delta_sum + 1) - c_4 + 8 * delta_prod * nu * (2 * nu + 1)
+                recursion_coeffs[0] -= 2 * (delta + k - 1) * (c_2 * (2 * nu + 1) + 2 * delta_prod * (6 * nu - 1) + 8 * delta_sum * (c_2 + nu - 2 * nu * nu))
+                recursion_coeffs[0] += 2 * (delta + k - 1) * (delta + k - 2) * (c_2 + nu - 2 * nu * nu + 4 * delta_prod + 12 * delta_sum * (1 - 2 * nu))
+                recursion_coeffs[0] += 2 * (delta + k - 1) * (delta + k - 2) * (delta + k - 3) * (2 * nu - 1 + 8 * delta_sum)
+                recursion_coeffs[0] -= 1 * (delta + k - 1) * (delta + k - 2) * (delta + k - 3) * (delta + k - 4)
+                recursion_coeffs[1] += 3 * c_4 + 2 * c_2 * (4 * delta_sum * (4 * delta_sum + 2 * nu + 1) + 2 * nu - 3) - 8 * delta_prod * (2 * delta_sum * (1 - 6 * nu) + 6 * nu * nu - 5 * nu)
+                recursion_coeffs[1] -= 2 * (delta + k - 2) * (2 * delta_prod * (16 * delta_sum - 10 * nu + 4) + 8 * delta_sum * (c_2 + nu - 2 * nu * nu) + (1 - 2 * nu) * (c_2 + 2 * nu - 2 + 32 * delta_sum * delta_sum))
+                recursion_coeffs[1] -= 2 * (delta + k - 2) * (delta + k - 3) * (3 * c_2 + 7 * nu + 2 * nu * nu - 10 + 4 * delta_prod + 4 * delta_sum * (10 * delta_sum + 6 * nu - 3))
+                recursion_coeffs[1] += 2 * (delta + k - 2) * (delta + k - 3) * (delta + k - 4) * (7 - 2 * nu + 8 * delta_sum)
+                recursion_coeffs[1] += 3 * (delta + k - 2) * (delta + k - 3) * (delta + k - 4) * (delta + k - 5)
+                recursion_coeffs[2] += 3 * c_4 + 2 * c_2 * (16 * delta_sum * delta_sum + 2 * nu - 3) + 16 * delta_prod * delta_sum * (8 * delta_sum + 2 * nu + 5)
+                recursion_coeffs[2] -= 2 * (delta + k - 3) * ((1 - 2 * nu) * (c_2 + 2 * nu - 2 + 32 * delta_sum * delta_sum - 4 * delta_prod) - 8 * delta_sum * (8 * delta_sum * delta_sum + 4 * delta_prod + 4 * nu * nu + 2 * c_2 - 5))
+                recursion_coeffs[2] -= 2 * (delta + k - 3) * (delta + k - 4) * (8 * delta_prod + 40 * delta_sum * delta_sum + 48 * delta_sum + 3 * c_2 + 2 * nu * nu + 7 * nu - 10)
+                recursion_coeffs[2] += 2 * (delta + k - 3) * (delta + k - 4) * (delta + k - 5) * (7 - 2 * nu - 16 * delta_sum)
+                recursion_coeffs[2] += 3 * (delta + k - 3) * (delta + k - 4) * (delta + k - 5) * (delta + k - 6)
+                recursion_coeffs[3] -= 3 * c_4 + 2 * c_2 * (16 * delta_sum * delta_sum + 2 * nu - 3) + 16 * delta_prod * delta_sum * (8 * delta_sum + 2 * nu + 5)
+                recursion_coeffs[3] -= 2 * (delta + k - 4) * (12 + 4 * nu - 8 * nu * nu - c_2 * (2 * nu + 5) - 8 * delta_sum * (8 * delta_sum * delta_sum + 8 * delta_sum * nu + 6 * delta_sum + 4 * nu * nu + 2 * c_2 - 5) + 4 * delta_prod * (2 * nu - 5 - 8 * delta_sum))
+                recursion_coeffs[3] -= 2 * (delta + k - 4) * (delta + k - 5) * (3 * c_2 + 2 * nu * nu + 7 * nu - 10 + 8 * delta_prod + delta_sum * (40 * delta_sum - 18 * nu + 21))
+                recursion_coeffs[3] -= 2 * (delta + k - 4) * (delta + k - 5) * (delta + k - 6) * (16 * delta_sum + 2 * nu + 11)
+                recursion_coeffs[3] -= 3 * (delta + k - 4) * (delta + k - 5) * (delta + k - 6) * (delta + k - 7)
+                recursion_coeffs[4] -= 3 * c_4 + 2 * c_2 * (4 * delta_sum * (4 * delta_sum + 2 * nu + 1) + 2 * nu - 3) - 8 * delta_prod * (2 * delta_sum * (1 - 6 * nu) + 6 * nu * nu - 5 * nu)
+                recursion_coeffs[4] -= 2 * (delta + k - 5) * (12 + 4 * nu - 8 * nu * nu - c_2 * (2 * nu + 5 - 8 * delta_sum) + 2 * delta_prod * (3 - 10 * nu + 16 * delta_sum) - 8 * delta_sum * (2 * nu * nu + 5 * nu + 3 + 8 * delta_sum * nu + 6 * delta_sum))
+                recursion_coeffs[4] -= 2 * (delta + k - 5) * (delta + k - 6) * (22 + 5 * nu - 2 * nu * nu - 3 * c_2 - 4 * delta_prod - 4 * delta_sum * (10 * delta_sum + 6 * nu + 9))
+                recursion_coeffs[4] += 2 * (delta + k - 5) * (delta + k - 6) * (delta + k - 7) * (8 * delta_sum - 2 * nu - 11)
+                recursion_coeffs[4] -= 3 * (delta + k - 5) * (delta + k - 6) * (delta + k - 7) * (delta + k - 8)
+                recursion_coeffs[5] -= 2 * c_2 * (2 * nu + 1) * (4 * delta_sum + 1) - c_4 + 8 * delta_prod * nu * (2 * nu + 1)
+                recursion_coeffs[5] -= 2 * (delta + k - 6) * ((2 * nu + 3) * (c_2 - 2 * nu - 2) + 6 * delta_prod * (2 * nu + 1) + 8 * delta_sum * (c_2 - 2 * nu * nu - 5 * nu - 3))
+                recursion_coeffs[5] -= 2 * (delta + k - 6) * (delta + k - 7) * (c_2 + 4 * delta_prod - (2 * nu + 3) * (nu + 4 + 12 * delta_sum))
+                recursion_coeffs[5] += 2 * (delta + k - 6) * (delta + k - 7) * (delta + k - 8) * (2 * nu + 5 + 8 * delta_sum)
+                recursion_coeffs[5] += 1 * (delta + k - 6) * (delta + k - 7) * (delta + k - 8) * (delta + k - 9)
+                recursion_coeffs[6] = (k + 2 * nu - 5) * (2 * delta + k - 7) * (delta + k - l - 6) * (delta + k + l + 2 * nu - 6)
+                recursion_coeffs[5] = recursion_coeffs[5].subs(ell, l)
+                recursion_coeffs[4] = recursion_coeffs[4].subs(ell, l)
+                recursion_coeffs[3] = recursion_coeffs[3].subs(ell, l)
+                recursion_coeffs[2] = recursion_coeffs[2].subs(ell, l)
+                recursion_coeffs[1] = recursion_coeffs[1].subs(ell, l)
+                recursion_coeffs[0] = recursion_coeffs[0].subs(ell, l)
 
                 pole_prod = 1
                 frob_coeffs.append(0)
