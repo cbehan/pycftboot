da52438f7082d4d0ac0865a61f6418d14f9bae15
Connor Behan <connor.behan@gmail.com>
2020-03-01 03:43:40 +0000

    Perform fewer workarounds for scalar blocks
    The option to natively compute a and b derivatives has been around for
    awhile. If we just force the user to supply this option, we can trim the
    code by quite a bit.
    
    Signed-off-by: Connor Behan <connor.behan@gmail.com>

--- compat_scalar_blocks.py
+++ compat_scalar_blocks.py
@@ -1,70 +1,3 @@
-class ConvolvedBlockTable2:
-    """
-    This is exactly like `ConvolvedBlockTable` except it uses derivatives with
-    respect to (z, zb) instead of (a, b).
-    """
-    def __init__(self, block_table, odd_spins = True, symmetric = False):
-        self.dim = block_table.dim
-        self.k_max = block_table.k_max
-        self.l_max = block_table.l_max
-        self.m_max = block_table.m_max
-        self.n_max = block_table.n_max
-        self.delta_12 = block_table.delta_12
-        self.delta_34 = block_table.delta_34
-        self.m_order = []
-        self.n_order = []
-        self.table = []
-
-        if odd_spins == False and block_table.odd_spins == True:
-            self.odd_spins = False
-        else:
-            self.odd_spins = block_table.odd_spins
-        if block_table.odd_spins == True:
-            step = 1
-        else:
-            step = 2
-        if self.odd_spins == True:
-            spin_list = range(0, self.l_max + 1, 1)
-        else:
-            spin_list = range(0, self.l_max + 1, 2)
-
-        symbol_array = []
-        for m in range(0, block_table.m_max + 1):
-            symbol_list = []
-            for n in range(0, min(m, block_table.m_max - m) + 1):
-                symbol_list.append(Symbol('g_' + m.__str__() + '_' + n.__str__()))
-            symbol_array.append(symbol_list)
-
-        derivatives = []
-        for m in range(0, block_table.m_max + 1):
-            for n in range(0, min(m, block_table.m_max - m) + 1):
-                if (symmetric == False and (m + n) % 2 == 0) or (symmetric == True and (m + n) % 2 == 1):
-                    continue
-
-                self.m_order.append(m)
-                self.n_order.append(n)
-
-                expression = 0
-                old_coeff = RealMPFR("0.25", prec) ** delta_ext
-                for i in range(0, m + 1):
-                    coeff = old_coeff
-                    for j in range(0, n + 1):
-                        expression += coeff * symbol_array[max(m - i, n - j)][min(m - i, n - j)]
-                        coeff *= 2 * (j - delta_ext) * (n - j) / (j + 1)
-                    old_coeff *= 2 * (i - delta_ext) * (m - i) / (i + 1)
-                deriv = expression / (factorial(m) * factorial(n))
-                derivatives.append(deriv)
-
-        for spin in spin_list:
-            l = spin // step
-            new_derivs = []
-            for i in range(0, len(derivatives)):
-                deriv = derivatives[i]
-                for j in range(len(block_table.table[l].vector) - 1, 0, -1):
-                    deriv = deriv.subs(symbol_array[max(block_table.m_order[j], block_table.n_order[j])][min(block_table.m_order[j], block_table.n_order[j])], block_table.table[l].vector[j])
-                new_derivs.append(2 * deriv.subs(symbol_array[0][0], block_table.table[l].vector[0]))
-            self.table.append(PolynomialVector(new_derivs, [spin, 0], block_table.table[l].poles))
-
 def scalar_blocks_read(block_table, name):
     """
     This reads in a block table produced by scalar_blocks, the program by Walter
@@ -76,6 +9,14 @@ def scalar_blocks_read(block_table, name):
     files = sorted(files0, key = len)
     # A cheap way to get alphanumeric sort
     info = files[0]
+
+    # The convolution functions to support both can be found in the git history
+    if info[:13] == "zzbDerivTable":
+        print("Please rerun scalar_blocks with --output-ab")
+        return
+    elif info[:12] != "abDerivTable":
+        print("Unknown convention for derivatives")
+        return
 
     # Parsing is annoying because '-' is used in the numbers and the delimiters
     delta12_negative = info.split("-delta12--")
@@ -97,13 +38,13 @@ def scalar_blocks_read(block_table, name):
     block_table.dim = float(info[1][1:])
     block_table.k_max = int(info[8][13:])
     block_table.n_max = int(info[7][4:]) - 1
-    block_table.m_max = 2 * block_table.n_max + 1
+    block_table.m_max = 1
     block_table.l_max = len(files) - 1
     block_table.odd_spins = False
     block_table.m_order = []
     block_table.n_order = []
-    for m in range(0, block_table.m_max + 1):
-        for n in range(0, min(m, block_table.m_max - m) + 1):
+    for n in range(0, block_table.n_max + 1):
+        for m in range(0, 2 * (block_table.n_max - n) + 2):
             block_table.m_order.append(m)
             block_table.n_order.append(n)
 
@@ -118,7 +59,7 @@ def scalar_blocks_read(block_table, name):
             block_table.l_max = l
         derivatives = []
         vector = open(full, 'r').read().replace('{', '').replace('}', '')
-        vector = re.sub("zzbDeriv\[[0-9]+,[0-9]+\]", "", vector).split(',\n')
+        vector = re.sub("abDeriv\[[0-9]+,[0-9]+\]", "", vector).split(',\n')
         for el in vector:
             poly = 0
             poly_lines = el.split('\n')
@@ -132,7 +73,7 @@ def scalar_blocks_read(block_table, name):
                     coeff = poly_lines[k].split('*')[0][5:]
                 poly += RealMPFR(coeff, prec) * (delta ** k)
             # It turns out that the scalars come with a shift of d - 2 which is not the unitarity bound
-            # All shifts, scalar or not, are undone here as we prefer to have this handle this step during XML writing
+            # All shifts, scalar or not, are undone here as we prefer to handle this step during XML writing
             derivatives.append(poly.subs(delta, delta - block_table.dim - l + 2).expand())
 
         # The block for scalar exchange should not give zero for the identity
